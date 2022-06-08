7730da3359b4ce18f4f252109d2d37ad8ce2150f
Walansito <88056064+emiliofurrer00@users.noreply.github.com>
2022-06-08 15:09:47 -0300

    added red border indication when required fields are left empty on TaskForm component

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import { StyledForm } from './TaskForm.styles'
+import { ErrorText, Input, Select, StyledForm } from './TaskForm.styles'
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
@@ -36,24 +36,24 @@ function TaskForm() {
     return (
         <section>
             <h2 className="heading-title">Crear tarea</h2>
-            <p>Creá y modificá una nueva tarea</p>
+            <p style={{marginBottom: 10}}>Creá y modificá una nueva tarea</p>
             <StyledForm onSubmit={handleSubmit}>
-                    <input name="title" placeholder="Titulo" onChange={handleChange}/>
-                    {errors.title && <p>{errors.title}</p>}
-                    <select name="status" onChange={handleChange}>
+                    <Input name="title" placeholder="Titulo" onChange={handleChange} error={errors.title !== undefined}/>
+                    {errors.title && <ErrorText>{errors.title}</ErrorText>}
+                    <Select name="status" onChange={handleChange} error={errors.status !== undefined}>
                         <option value="">Seleccionar estado</option>
                         <option value="new">Nuevo</option>
                         <option value="inProcess">En proceso</option>
                         <option value="finished">Finalizado</option>
-                    </select>
-                    { errors.status && <p>{errors.status}</p>}
-                    <select name="priority" onChange={handleChange}>
+                    </Select>
+                    {errors.status && <ErrorText>{errors.status}</ErrorText>}
+                    <Select name="priority" onChange={handleChange} error={errors.priority !== undefined}>
                         <option>Seleccionar prioridad</option>
                         <option value="low">Probando</option>
                         <option value="medium">Probando</option>
                         <option value="high">Probando</option>
-                    </select>
-                    { errors.priority && <p>{errors.priority}</p>}
+                    </Select>
+                    {errors.priority && <ErrorText>{errors.priority}</ErrorText>}
                     <textarea placeholder="Descripción (opcional)" onChange={handleChange}/>
                     <button type="submit">Crear</button>
             </StyledForm>
