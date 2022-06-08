caac22c9eafac2a7bbf02e56e96003959b4c447e
Walansito <88056064+emiliofurrer00@users.noreply.github.com>
2022-06-08 14:54:29 -0300

    integrated functioning login with rest api

--- src/components/views/Login/Login.jsx
+++ src/components/views/Login/Login.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useFormik } from 'formik'
 import { Link, useNavigate } from 'react-router-dom';
 import './Login.styles.css'
+
+async function handleLogin(userName, password){
+    try {
+        const response = await fetch('https://goscrum-api.alkemy.org/auth/login', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'  
+            },
+            body: JSON.stringify({
+                    userName,
+                    password,
+            })
+        })
+        const json = await response.json();
+        return json
+    } catch(e){
+        console.error(e)
+    }
+}
 
 const Login = () => {
     const navigate = useNavigate();
 
     const initialValues = {
-        email: "",
+        username: "",
         password: "",
     };
 
     const validate = values => {
         const errors = {};
-        if(!values.email){
-            errors.email = "El email es requerido.";
+        if(!values.username){
+            errors.username = "El nombre de usuario es requerido.";
         }
         if (!values.password){
             errors.password = "El password es requerido.";
@@ -22,9 +41,22 @@ const Login = () => {
         return errors;
     }
 
-    const onSubmit = () => {
-        localStorage.setItem("logged", "yes");
-        navigate("/", {replace: true});
+    const [loginError, setLoginError] = useState("");
+
+    const onSubmit = async (values, actions) => {
+        const {username, password} = values;
+        //console.log(values);
+        const response = await handleLogin(username, password);
+        //console.log(response)
+        if (response.status_code === 200) {
+            localStorage.setItem("logged", "yes");
+            localStorage.setItem("token", response.result.token);
+            navigate('/', {replace: true});
+        } else if (response.status_code === 404) {
+            setLoginError("No existe cuenta asociada con ese nombre de usuario.")
+        } else if (response.status_code === 401) {
+            setLoginError("Contraseña incorrecta.")
+        }
     }
 
     const formik = useFormik({
@@ -40,14 +72,14 @@ const Login = () => {
             <form onSubmit={handleSubmit}>
             <h2 className="title">Iniciar sesión</h2>
             <div>
-                <label>Email</label>
+                <label>Nombre de usuario</label>
                 <input 
-                    value={values.email} 
-                    name="email" 
-                    type="email" 
+                    value={values.username} 
+                    name="username" 
+                    type="text" 
                     onChange={handleChange} 
                 />
-                {errors.email && <p>{errors.email}</p>}
+                {errors.username && <p>{errors.username}</p>}
             </div>
             <div>
                 <label>Contraseña</label>
@@ -59,7 +91,8 @@ const Login = () => {
                 />
                 {errors.password && <p>{errors.password}</p>}
             </div>
-            <input type="submit"/>            
+            <input type="submit"/>
+            {loginError && <p style={{color: 'red'}}>{loginError}</p>}            
             <Link className="link" to="/register">Registrarme</Link>
             </form>
         </div>
